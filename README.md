6f9b95149df02942861b26d735319d643a8e542a
Cortexture <marioacabada@gmail.com>
2023-12-14 05:05:28 -0600

    added the link to the functional programming guide for julia

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # assemb_lingua
 A corpus agnostic keyword extraction using assembly theory written in Julia.
+
+https://marketsplash.com/tutorials/julia/julia-functional-programming/
--- src/main.jl
+++ src/main.jl
@@ -2,7 +2,8 @@ file = open("../data/exploring_the_wonders_of_nature.txt")
 text_vector = readlines(file);
 close(file)
 
-println(sizeof(text_vector[1]))
+struct text 
 
-# use DelimitedFiles.readdlm
-
+for word ∈ text_vector
+    println("<$word>\n")
+end
