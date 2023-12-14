f3b2c884fff9769df9cbfd777350c1013ce06a13
Cortexture <marioacabada@gmail.com>
2023-12-14 09:08:56 -0600

    cleaned up the process, realized you don't need a function

--- src/main.jl
+++ src/main.jl
@@ -2,15 +2,9 @@ file = open("../data/exploring_the_wonders_of_nature.txt")
 text_vector = readlines(file);
 close(file)
 
-function get_words(_text) 
-    sans_special_char = split(_text[1], "—")
-    strings_rejoined = [sans_special_char[1] * " " * sans_special_char[2]]
-    punctuation_removed = replace(strings_rejoined[1],
-                                  "," => "", "." => "", "'" => "")
-    _words = split(punctuation_removed)
-end
-
-words = get_words(text_vector)
+words = replace(text_vector[1],
+                  "," => "", "." => "", "'" => "", 
+                  "—" => " ") |> split
 
 # now need to get IPA encodings for the words here
 # then re-add the punctuation
