54a0b4ace23ba9eb1b7b0bb57d30e790a0e3244e
Cortexture <marioacabada@gmail.com>
2023-12-14 09:45:11 -0600

    made to uppercase to match the cmu dataset

--- src/main.jl
+++ src/main.jl
@@ -1,11 +1,12 @@
-file = open("../data/exploring_the_wonders_of_nature.txt")
-text_vector = readlines(file);
-close(file)
+using DataFrames
 
+text_vector = open("../data/exploring_the_wonders_of_nature.txt") |> readlines
 words = replace(text_vector[1],
                   "," => "", "." => "",
-                  "—" => " ") |> split
+                  "—" => " ") |> split .|> uppercase
 
 # now need to get IPA encodings for the words here
 # then re-add the punctuation
+
+cmu_text = open("../data/cmudict-0.7b") |> readlines
 
