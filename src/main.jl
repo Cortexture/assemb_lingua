ce04db6f22487777981be003ce28e1c4e955c8ca
Cortexture <marioacabada@gmail.com>
2023-12-14 11:13:04 -0600

    doing some debugging

--- src/main.jl
+++ src/main.jl
@@ -1,11 +1,19 @@
-using DataFrames
-
 text_vector = open("../data/exploring_the_wonders_of_nature.txt") |> readlines
 words = replace(text_vector[1],
                   "," => "", "." => "",
                   "—" => " ") |> split .|> uppercase
 
-cmu_text = open("../data/cmudict-0.7b") |> readlines
+cmu_dataset = open("../data/cmudict-0.7b") |> readlines
+
+entries = split.(cmu_dataset, "  ")
+
+println("The type of entries: ", typeof(entries))
+println("The size of entries: ", size(entries))
+println("The type of entries[1]: ", typeof(entries[1]))
+println("The size of entries[1]: ", size(entries[1]))
+
+# ARPAbet_dictionary = set_dictionary(cmu_dataset)
+
 
 # need to remove the punctuation lines and the header of the text
 
