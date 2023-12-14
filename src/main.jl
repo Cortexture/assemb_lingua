5ade9164a7a8a6e7d00bee6d20866556033339f3
Cortexture <marioacabada@gmail.com>
2023-12-14 09:46:42 -0600

    coming up with game plan

--- src/main.jl
+++ src/main.jl
@@ -5,8 +5,11 @@ words = replace(text_vector[1],
                   "," => "", "." => "",
                   "—" => " ") |> split .|> uppercase
 
+cmu_text = open("../data/cmudict-0.7b") |> readlines
+
+# need to remove the punctuation lines and the header of the text
+
 # now need to get IPA encodings for the words here
 # then re-add the punctuation
 
-cmu_text = open("../data/cmudict-0.7b") |> readlines
 
